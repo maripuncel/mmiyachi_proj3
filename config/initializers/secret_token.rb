d9d622fff33335069272d2be77e241bb76cd5c27
Mari Miyachi <mmiyachi@mit.edu>
2012-10-26 13:00:02 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmiyachiProj3::Application.config.secret_token = '894d6dd45c8fc74aa475adf6b0032ecae2975a126cd83a6f3b3c3c01174d1ee336abdde61c927c78ef77836347cb608b9d6a225db85675723fbfcc21b356165a'
