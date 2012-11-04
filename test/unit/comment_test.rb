54ecd3e8b8afe04a344b201e812040bc919c73ae
Mari Miyachi <mmiyachi@mit.edu>
2012-11-04 14:24:23 -0800

    tests added

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :comments, :admins, :posts
+
+  # test if comment can be created
+  def test_create
+    post = posts(:one)
+    comment = comments(:one)
+    new_comment = post.comments.create(:author => comment.author)
+    assert_equal comment.author, new_comment.author
+  end
+
+  # test setting the priority of comments
+  def test_priority
+    post = posts(:one)
+    comment = post.comments.create(:author => "Mari")
+    comment.set_priority
+    assert_equal 1, comment.priority
+  end
 end
